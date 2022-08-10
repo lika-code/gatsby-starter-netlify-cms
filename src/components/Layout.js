a169494344945a41018b896dc9ea4dad25ebef7e
Anzhelika <anzhelika.designer35@gmail.com>
2022-08-10 13:48:17 +0200

    fix

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -28,7 +28,7 @@ const TemplateWrapper = ({ children }) => {
     "EmailIsMissing";
 
   console.log(email);
-  console.log(JSON.stringify(identity.user));
+  console.log(JSON.stringify(identity.user.email));
 
   return (
     <div>
